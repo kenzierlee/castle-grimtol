262920e2e57e50e1193adc31704fcc88abc3274e
Mackenzie lee <kenzierlee23@outlook.com>
2018-03-28 17:09:07 -0600

    adding use item functionality

--- Project/Game.cs
+++ Project/Game.cs
@@ -7,6 +7,7 @@ namespace CastleGrimtol.Project
     {
         public Room CurrentRoom { get; set; }
         public Player CurrentPlayer { get; set; }
+        public bool Map { get; set; }
         // public Dictionary<string, Game> Directions { get; set; }
         public void Setup()
         {
@@ -18,7 +19,117 @@ namespace CastleGrimtol.Project
         }
         public void UseItem(string itemName)
         {
+            var input = itemName.ToLower();
+            bool success = false;
+            for (var i = 0; i < CurrentPlayer.Inventory.Count; i++)
+            {
+                string item = CurrentPlayer.Inventory[i].Name.ToLower();
+                if (item == itemName)
+                {
+                    success = true;
+                }
+            }
+            if (success && input == "paper" && Map && CurrentRoom.Name != "The Crypt")
+            {
+                System.Console.WriteLine(@"
+      N
+    W   E
+      S
 
+        Broom Closet -----------------N. Hallway-----------------|
+                |        |                                              *
+                |-----Nursery                                           |
+            W. Hallway                                                  |
+                |                                                  E. Hallway
+ Trophy Room----|           Wine Cellar                                 |
+                |                |                                      |   
+              Crypt----------------------S. Hallway----------------------
+                |                                      |
+           Crematorium                          Walk in Freezer
+                ");
+            }
+            //             if (CurrentRoom.Name == "Walk In Freezer")
+            //             {
+            //                 System.Console.WriteLine("Hurry enter a four digit code!");
+            //                 string input = Console.ReadLine();
+            //                 int code = 0;
+            //                 bool valid = int.TryParse(input, out code);
+            //                 if (valid)
+            //                 {
+            //                     if (code == 1121)
+            //                     {
+            //                         System.Console.WriteLine("The door opens");
+            //                     }
+            //                     else
+            //                     {
+            //                         System.Console.WriteLine(@"
+            // You entered the wrong code, and eventually freeze 
+            // to death...");
+            //                     }
+            //                 }
+            //                 else
+            //                 {
+            //                     System.Console.WriteLine(@"
+            // You entered the wrong code, and eventually freeze 
+            // to death...");
+            //                 }
+            //             }
+            if (success && CurrentRoom.Name == "Upstairs")
+            {
+                if (input == "key")
+                {
+                    System.Console.WriteLine(@"
+You try the door, its locked. You use the key 
+you found in the north Hallway to unlock the door, 
+and much to your surprise it works! You slowly 
+open the door and realize its not over yet. You 
+need to sneek through the main house and get out 
+so you can get help");
+                }
+                else
+                {
+                    System.Console.WriteLine(@"
+You go to open the door but its locked, you 
+hear footsteps rushing towards you. He heard 
+you try to open the door, you turn around to 
+run but its too late. Your dead...");
+                }
+            }
+            if (success && CurrentRoom.Name == "The Back Door")
+            {
+                if (itemName == "Pocket Watch")
+                {
+                    System.Console.WriteLine(@"
+You pull out the pocket watch you found from the 
+house, and ask if he will accept it as payment");
+                    System.Console.WriteLine(@"
+The cab driver agrees. You get in the cab and 
+take your first deep breathe. Its over, You Won!");
+                }
+                else
+                {
+                    System.Console.WriteLine(@"
+You inform the cab driver you will pay him once you 
+get there. The cab driver replies angrily at your 
+response and drives off. Your kidnapper's caught up 
+by now, he shoots at you from behind. You fall to the 
+ground. So close... You lost.");
+                }
+            }
+            else if(success)
+            {
+                System.Console.WriteLine("Unable to use that item at this time");
+            }
+            else
+            {
+                System.Console.WriteLine(@"
+You dont not have that item in your inventory enter 
+inventory to see what items you have");
+            }
+            // if (CurrentRoom.Name == "The Crematorium" || CurrentRoom.Name == "The Front Door")
+            // {
+            //     SearchRoom();
+            // }
         }
         public void ListInventory()
         {
@@ -58,8 +169,13 @@ namespace CastleGrimtol.Project
                 string name = CurrentRoom.Items[i].Name.ToLower();
                 if (name == itemName)
                 {
+                    if (itemName == "paper")
+                    {
+                        Map = true;
+                    }
                     CurrentPlayer.Inventory.Add(CurrentRoom.Items[i]);
                     System.Console.WriteLine("Item Added to Your Inventory");
+                    System.Console.WriteLine(CurrentRoom.Items[i].Description);
                     valid = true;
                 }
             }
@@ -72,7 +188,6 @@ namespace CastleGrimtol.Project
         public void updateCurrentRoom(string direction)
         {
             Console.Clear();
-            SearchRoom();
             if (CurrentRoom.Name == "The Crypt")
             {
                 bool valid = false;
@@ -81,6 +196,7 @@ namespace CastleGrimtol.Project
                     string item = CurrentPlayer.Inventory[i].Name;
                     if (item == "Wrench")
                     {
+                        System.Console.WriteLine("You use the wrench to loosen the pipe and leverage against it to break free. You quickly run to the door and open it...");
                         CurrentRoom = CurrentRoom.Directions[direction];
                         valid = true;
                         SearchRoom();
@@ -88,10 +204,10 @@ namespace CastleGrimtol.Project
                 }
                 if (!valid)
                 {
-                    System.Console.WriteLine("The Door is locked, it looks like you need to find a key to unlock it");
+                    System.Console.WriteLine("You cant get out of the room, you need to get yourself free from the pipe.");
                 }
             }
-            if (CurrentRoom.Directions[direction] == CurrentRoom.Directions[direction])
+            else if (CurrentRoom.Directions[direction] != null)
             {
                 CurrentRoom = CurrentRoom.Directions[direction];
                 SearchRoom();
@@ -104,7 +220,7 @@ namespace CastleGrimtol.Project
         public void SearchRoom()
         {
             System.Console.WriteLine($@"
-            Room Name: {CurrentRoom.Name}
+Room Name: {CurrentRoom.Name}
             {CurrentRoom.Description}");
         }
         public Game(Room currentRoom, Player currentPlayer)
--- Quest.cs
+++ Quest.cs
@@ -15,79 +15,170 @@ namespace CastleGrimtol
                 Room northHallway = new Room()
                 {
                     Name = "The north Hallway",
-                    Description = "Another long hallway, only this ones filled with shattered glass scattered all across the floor. You see something shinning at the end of it... It looks like a key? Hmmm..."
+                    Description = @"
+Another long hallway, only this ones filled 
+with shattered glass scattered all across the floor. You 
+see something shinning at the end of it... It looks like 
+a key? Hmmm..."
                 };
                 Room westHallway = new Room()
                 {
                     Name = "The west Hallway",
-                    Description = "Its a long narrow hallway, you hear yelling in the distance followed by what sounded like 3 gunshots. You start running towards what looks to be a source of light. You trip, when you like to see what you stumbled on you discover its an arm... Just an arm, clutched in the arms hand looks to be a pocket watch."
+                    Description = @"
+Its a long narrow hallway, you hear 
+yelling in the distance followed by what sounded 
+like 3 gunshots. You start running towards what looks 
+to be a source of light. You trip, when you like to see 
+what you stumbled on you discover its an arm... Just an 
+arm, clutched in the arms hand looks to be a pocket watch."
                 };
                 Room southHallway = new Room()
                 {
                     Name = "The south Hallway",
-                    Description = "The hallways stacked high with what seems to be clothing, and shoes. You hear someone coming you need to hurry up they're getting closer. You crawl over the waist high piles of clothing for what seems to be forever, you reach for something to help you along and notice a shimmering object amidst the blood stained clothes. You take a closer look and notice its a lighter. You finally reach the end of the hallway. You gather yourself and stand up. Quick where to next???"
+                    Description = @"
+The hallways stacked high with what 
+seems to be clothing, and shoes. You hear someone coming 
+you need to hurry up they're getting closer. You crawl 
+over the waist high piles of clothing for what seems to 
+be forever, you reach for something to help you along and 
+notice a shimmering object amidst the blood stained clothes. 
+You take a closer look and notice its a lighter. You finally 
+reach the end of the hallway. You gather yourself and stand 
+up. Quick where to next???"
                 };
                 Room eastHallway = new Room()
                 {
                     Name = "The east Hallway",
-                    Description = "The Hallway is at a steep incline, you get on your hands and knees to attempt the climb."
+                    Description = @"
+The Hallway is at a steep incline, you 
+get on your hands and knees to attempt the climb."
                 };
                 Room lab = new Room()
                 {
                     Name = "The Lab",
-                    Description = "The room is poorly lit but you can make out what looks to be a lab. There's a table filled with surgical tools, and a table in the center of the room with what looks to be restraints for arms. You panic as you fear you will be the next victim strapped to the table as someone performs some sort of experiment on you. You quickly look around the room for something that might help you get out of this nightmare of a place. All you see is eerie World War 2 memorabilia, including a gas mask."
+                    Description = @"
+The room is poorly lit but you can make 
+out what looks to be a lab. There's a table filled with 
+surgical tools, and a table in the center of the room with 
+what looks to be restraints for arms. You panic as you fear 
+you will be the next victim strapped to the table as someone 
+performs some sort of experiment on you. You quickly look 
+around the room for something that might help you get out of 
+this nightmare of a place. All you see is eerie World War 2 
+memorabilia, including a gas mask."
                 };
                 Room broomCloset = new Room()
                 {
                     Name = "Broom Closet",
-                    Description = "It seems you've entered into a closet filled with cleaning supplies. You take a quick look around the room, and notice a walkie talkie. Nothing useful in here, you're waisting time. Hurry where to next???"
+                    Description = @"
+It seems you've entered into a closet filled 
+with cleaning supplies. You take a quick look around the room, 
+and notice a walkie talkie. Nothing useful in here, you're 
+waisting time. Hurry where to next???"
                 };
                 Room nursery = new Room()
                 {
                     Name = "Nursery",
-                    Description = "You feel along the walls for a light switch, you find it and turn the lights on. The fluorescent light flickers, as you get a glimpse of what seems to be a baby's nursery. Thats when you notice the crib in the corner of the room... It's rocking back and forth. Out of the corner of your eye you notice a rusty old hammer and some wood. It looks like someone was trying to make a changing table for the nursery."
+                    Description = @"
+You feel along the walls for a light switch, 
+you find it and turn the lights on. The fluorescent light 
+flickers, as you get a glimpse of what seems to be a baby's 
+nursery. Thats when you notice the crib in the corner of the 
+room... It's rocking back and forth. Out of the corner of 
+your eye you notice a rusty old hammer and some wood. It looks 
+like someone was trying to make a changing table for the nursery."
                 };
                 Room crematorium = new Room()
                 {
                     Name = "The Crematorium",
-                    Description = "Its rather warm in here, you see what looks to a furnace, you get closer and realize you're in a Crematory. Where are you??? Your hit over the head, and meet your fate. All evidence of you is insenerated and you are never to be seen or heard from again..."
+                    Description = @"
+Its rather warm in here, you see what 
+looks to a furnace, you get closer and realize you're 
+in a Crematory. Where are you??? Your hit over the head, 
+and meet your fate. All evidence of you is insenerated 
+and you are never to be seen or heard from again..."
                 };
                 Room trophyRoom = new Room()
                 {
                     Name = "The Trophy Room",
-                    Description = "It appears to be a room with lined with shelves, filled with trophies and awards. You read a few and notice all the awards are made out to a Samuel Smith. This must be his house, he must be your kidnapper. The man behind all of this. But why? Why would he want you? You search the desk in the center of the room only to find a list. The list contains several dozen names and yours... Yours is one of them... You continue to look around and find nothing useful."
+                    Description = @"
+It appears to be a room with lined with 
+shelves, filled with trophies and awards. You read a few 
+and notice all the awards are made out to a Samuel Smith. 
+This must be his house, he must be your kidnapper. The man 
+behind all of this. But why? Why would he want you? You 
+search the desk in the center of the room only to find a list. 
+The list contains several dozen names and yours... Yours is 
+one of them... You continue to look around and find nothing useful."
                 };
                 Room wineCellar = new Room()
                 {
                     Name = "The Wine Cellar",
-                    Description = "For such a desturbing place this room has an amazing collection of wines. You try not to get side tracked by the wines, as you are havent had anything to drink in a long time."
+                    Description = @"
+For such a desturbing place this room has an 
+amazing collection of wines. You try not to get side tracked 
+by the wines, as you are havent had anything to drink in a 
+long time."
                 };
                 Room walkInFreezer = new Room()
                 {
                     Name = "Walk In Freezer",
-                    Description = "burrrrr It's freezing in here, you flip on the lights and oh my... Thats not... Thats not what you think it is... is it?? Are those... Are those bodies. You quickly turn back to leave and notice there is no handle to get out. Your stuck in here, where you'll soon freeze to death. Quick there has to be some way to get out of here. You notice a keypad next to the door. But what's the combination???"
+                    Description = @"
+burrrrr It's freezing in here, you flip on 
+the lights and oh my... Thats not... Thats not what you 
+think it is... is it?? Are those... Are those bodies. You 
+quickly turn back to leave and notice there is no handle to 
+get out. Your stuck in here, where you'll soon freeze to death. 
+Quick there has to be some way to get out of here. You notice 
+a keypad next to the door. But what's the combination???"
                 };
                 Room upstairs = new Room()
                 {
                     Name = "Upstairs",
-                    Description = "Could this be it? The door to your freedom. Could this nightmare really be over??"
+                    Description = @"
+Could this be it? The door to your freedom. 
+Could this nightmare really be over??"
                 };
                 Room frontDoor = new Room()
                 {
                     Name = "The Front Door",
-                    Description = "You can see it, a few feet away the front door. You walk towards it and the floor boards creek. You run to the door as surely he's heard. But your not fast enough, and you meet your fate..."
+                    Description = @"
+You can see it, a few feet away the front door. 
+You walk towards it and the floor boards creek. You run to the 
+door as surely he's heard. But your not fast enough, and you 
+meet your fate..."
                 };
                 Room backDoor = new Room()
                 {
                     Name = "The Back Door",
-                    Description = "You can see it, freedom just within your grasp you rush to the door. Running out to the street, searching for help. You see headlights heading in your direction. You wave your arms histarically hoping they'll stop. The car comes to an abrupt stop just in time. It's a cab the driver rolls down the window and asks where your headed you respond with the police station please. He proceeds to ask if you have money to pay for it. You have no money."
+                    Description = @"
+You can see it, freedom just within your grasp 
+you rush to the door. Running out to the street, searching for 
+help. You see headlights heading in your direction. You wave 
+your arms histarically hoping they'll stop. The car comes to an 
+abrupt stop just in time. It's a cab the driver rolls down the 
+window and asks where your headed you respond with the police 
+station please. He proceeds to ask if you have money to pay for it. 
+You have no money."
                 };
                 Room crypt = new Room()
                 {
-                    Name = "The Crypts",
-                    Description = "It's dark, you can hardly see in front of you. You notice a handcuff on your left wrist you follow the chain to what feels like a pipe. You yank the chain to attempt to free yourself to no avail. You feel around and notice a slip of paper on the ground, however its too dark in here to read the contents. You can hardly make it out but it looks as though a number sequence is written on the wall 1121. You hear footsteps that sound like they're coming from above you. You need to get out, you need to get help. You continue to search the room as best as you can for something to help get you loose from the restraint. All you notice is what feels like a wrench. Maybe you can leverage it against the pipe or try to loosen it so you can break free?"
+                    Name = "The Crypt",
+                    Description = @"
+It's dark, you can hardly see in front of you. 
+You notice a handcuff on your left wrist you follow the chain 
+to what feels like a pipe. You yank the chain to attempt to free 
+yourself to no avail. You feel around and notice a slip of paper 
+on the ground, however its too dark in here to read the contents. 
+You can hardly make it out but it looks as though a number sequence 
+is written on the wall 1121. You hear footsteps that sound like 
+they're coming from above you. You need to get out, you need to get 
+help. You continue to search the room as best as you can for 
+something to help get you loose from the restraint. All you notice 
+is what feels like a wrench. Maybe you can leverage it against the 
+pipe or try to loosen it so you can break free?"
                 };
-                crypt.CreateItem("Paper", "Its Mostly Scribbles you can't make out, but there is a word thats repeated often... Jessie");
+                crypt.CreateItem("Paper", "Its Mostly Scribbles you can't make out, but theres a pattern maybe when you get out of the room you'll be able to read it.");
                 crypt.CreateItem("Wrench", "A rusty steel wrench");
                 northHallway.CreateItem("Key", "A Key, but for what?");
                 westHallway.CreateItem("Pocket Watch", "It appears to be broken, Maybe its worth something.");
@@ -115,6 +206,7 @@ namespace CastleGrimtol
                 nursery.Directions.Add("north", northHallway);
                 upstairs.Directions.Add("north", frontDoor);
                 upstairs.Directions.Add("south", backDoor);
+                walkInFreezer.Directions.Add("north", southHallway);
                 return crypt;
             }
         }
@@ -126,7 +218,9 @@ namespace CastleGrimtol
         public void Start()
         {
             game = new Game(CreateRooms, UserSetup());
-            System.Console.WriteLine("You wake up confused and panic when you realize you have no idea where you are... You notice your shoes are gone?");
+            System.Console.WriteLine(@"
+You wake up confused and panic when you realize you have no idea 
+where you are... You notice your shoes are gone?");
         }
         //user options
         public void PlayerChoice()
@@ -178,7 +272,7 @@ namespace CastleGrimtol
                 validChoice = userInput;
                 game.ListDirections();
             }
-            else if (validChoice == "")
+            else if(validChoice.Length <1)
             {
                 System.Console.WriteLine("Your waisting time... Provide a valid choice. Enter 'help' if you need to see a list of valid options.");
                 PlayerChoice();
@@ -187,6 +281,25 @@ namespace CastleGrimtol
         //sets up the 
         public void GamePlay()
         {
+//             if(game.Map && game.CurrentRoom.Name != "The Crypt")
+//             {
+//                 System.Console.WriteLine(@"
+//       N
+//     W   E
+//       S
+
+//         Broom Closet -----------------N. Hallway-----------------|
+//                 |        |                                              *
+//                 |-----Nursery                                           |
+//             W. Hallway                                                  |
+//                 |                                                  E. Hallway
+//  Trophy Room----|           Wine Cellar                                 |
+//                 |                |                                      |   
+//               Crypt----------------------S. Hallway----------------------
+//                 |                                      |
+//            Crematorium                          Walk in Freezer
+//                 ");
+//             }
             if (game.CurrentRoom.Name == "Walk In Freezer")
             {
                 System.Console.WriteLine("Hurry enter a four digit code!");
@@ -226,7 +339,20 @@ namespace CastleGrimtol
                 if (success)
                 {
                     System.Console.WriteLine("You try the door, its locked. You use the key you found in the north Hallway to unlock the door, and much to your surprise it works! You slowly open the door and realize its not over yet. You need to sneek through the main house and get out so you can get help");
-                    PlayerChoice();
+                    bool valid = false;
+                    for (var i = 0; i < game.CurrentPlayer.Inventory.Count; i++)
+                    {
+                        string itemName = game.CurrentPlayer.Inventory[i].Name;
+                        if (itemName == "Walkie Talkie")
+                        {
+                            System.Console.WriteLine("You attempt to be quite, but the walkie talkie you grabbed from the broom closet goes off. You attempt to run to no avail. You meet your fate...");
+                            PlayAgain();
+                        }
+                    }
+                    if (valid)
+                    {
+                        PlayerChoice();
+                    }
                 }
                 else
                 {
@@ -259,6 +385,7 @@ namespace CastleGrimtol
             }
             if (game.CurrentRoom.Name == "The Crematorium" || game.CurrentRoom.Name == "The Front Door")
             {
+                game.SearchRoom();
                 PlayAgain();
             }
             else
